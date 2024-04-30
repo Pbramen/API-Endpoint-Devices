4e4a358541aca6d65f43fa69bda325c8b5648c33
Peter Banh <peterbanhe.e@gmail.com>
2024-04-30 12:01:31 -0500

    Added search by status with filters

--- search_equip.php
+++ search_equip.php
@@ -1,26 +1,11 @@
 <?php
-	if($active == null){
-		$active = 1;
-	}
-	//validate active here...
-	if($active != 0 && $active != 1){
-		handle_logger('log_API_error', $logger, 200, 'Invalid paramater given for active.', 'None taken', $endPoint, $time_start);
-		handleAPIResponse(200, "Invalid active param.", "", 'api/search_equip', $time_start);
-		exit();
-	}
 
-	if($limit == null){
-		$limit = 1000;
-	}
-	else if( is_numeric($limit) ){
-		$limit = strval($limit);
-		$limit = $limit < 1000 ? $limit : 1000;
-	}
-	else{
-		// log error here and exit
-	}
-	$params = array('attribute' => "",
-					'p' => "",
+	validPagination($logger, $limit, "maxRange", 1000, 'limit', $endPoint, $time_start);
+	validPagination($logger, $offset, "minRange", 0, 'offset', $endPoint, $time_start);
+	validActive($logger, $active, $endPoint, $time_start);
+
+	$params = array(
+					'p' => array(),
 					'bind' => "",
 					'values' => array()
 				   );
@@ -37,45 +22,42 @@
 		
 		if(isset($sn_json['Status']) &&$sn_json['Status'] == 200 && $sn_json['MSG'] == 'Success'){
 			//sn and sn id found...
-			prepareFilters($params, "sn", "i", "sn_id", $sn_json["Payload"]["Fields"]["id"]);
+			searchPrepare($params, "iii", 'AND sn.sn_id = (?)',  $sn_id, $limit, $offset);
 		}
 		else{
+			// TODO handle error
 			echo "system failure";
 			exit();
 		}
 	}
 	else{
-		$empty += 1;
+		applyNoFilter($extra, $params, $limit, $offset);
 	}
 	// if $d == 0 -> same as no company filter. Returns error msg on negative
 	if($d){
 		validateAPI($logger, $d, "device", $endPoint, $endPoint, $time_start);
-		prepareFilters($params, "relation", "i", "device_id", $d);
-		
-		// 
+		searchPrepare($params, "iii", "AND d.device_id = (?) ", $d, $limit, $offset);
 	}
 	else {
-		$empty += 1;
+		applyNoFilter($extra, $params, $limit, $offset);
 	}
 	// if $c == 0 -> same as no company filter. Returns error msg on negative.
 	if($c){ 
 		validateAPI($logger, $c, "company", $endPoint, $endPoint, $time_start);
-		prepareFilters($params, "relation", "i", "company_id", $c);
+		searchPrepare($params, "iii", "AND c.company_id = (?) ", $c, $limit, $offset);
 	}
 	else{
-		$empty += 1;
+		applyNoFilter($extra, $params, $limit, $offset);	
 	}
-
 	$sql = buildSQL($params, $active, $empty, $limit, $offset);
-	//echo $sql;
-	print_r($params['values']);
-
+	
 	try{	
 		$res = bindAndExecute($db, $sql, $params['bind'], $params['values']);
 		$r = $res->get_result();
 		$payload = array();
 		while($row = $r->fetch_assoc()){
-			array_push($payload, ['sn' => ['value' => $row['sn'], 'Action' => 'api/query_sn?sn='.$row['sn']], 
+			
+			array_push($payload, ['sn' => ['id' => $row['sn_id'], 'value' => $row['sn'], 'Action' => 'api/query_sn?sn='.$row['sn']], 
 								  'device' => ['id' =>$row['device_id'], 'Action' => 'api/query_device?d='.$row['device_id']], 
 								  'company' => ['id' => $row['company_id'], 'Action' => 'api/query_company?c='.$row['company_id']], 
 								  'r_id' => $row['r_id']]
@@ -100,35 +82,54 @@
 
 	
 	function buildSQL(&$params, $active, $empty, $limit, $offset){
+		$sn_active = "";
+		$d_active = "";
+		$c_active = "";
+		
+		if ($active == 1){
+			$sn_active = "WHERE sn.active = 1";
+			$d_active = "WHERE d.active = 1";
+			$c_active = "WHERE c.active = 1";
+		}
+		
 		if($empty == 3){
-			$params['bind'] = 'ii';
+			$params['bind'] = 'iiiiii';
 			$params['values'] = [$limit, $offset];
 
-			$sn_active = "";
-			$d_active = "";
-			$c_active = "";
-
-			if ($active){
-				$params['bind'] .= 'iiii';
-				array_push($params['values'], $limit);
-				array_push($params['values'], $offset);
-				array_push($params['values'], $limit);
-				array_push($params['values'], $offset);
-				$sn_active = "WHERE sn.active = 1";
-				$d_active = "WHERE d.active = 1";
-				$c_active = "WHERE c.active = 1";
-			}
+			array_push($params['values'], $limit);
+			array_push($params['values'], $offset);
+			array_push($params['values'], $limit);
+			array_push($params['values'], $offset);
+			
 			// Query Optimizer for mysql optimizer...
-			$sql = "(Select r.r_id, sn.sn_id, c.company, d.device from relation as r JOIN sn ON r.sn_id = sn.sn_id JOIN company as c ON r.company_id = c.company_id JOIN device as d ON d.device_id = r.device_id $sn_active LIMIT ? OFFSET ?) UNION ALL (Select r.r_id, sn.sn_id, c.company, d.device from relation as r JOIN sn ON r.sn_id = sn.sn_id JOIN company as c ON r.company_id = c.company_id JOIN device as d ON d.device_id = r.device_id $c_active LIMIT ? OFFSET ?) UNION ALL (Select r.r_id, sn.sn_id, c.company, d.device from relation as r JOIN sn ON r.sn_id = sn.sn_id JOIN company as c ON r.company_id = c.company_id JOIN device as d ON d.device_id = r.device_id $d_active LIMIT ? OFFSET ?)";
+			$sql = "(Select r.r_id, sn.sn_id, sn.sn, c.company_id, d.device_id from relation as r JOIN sn ON r.sn_id = sn.sn_id JOIN company as c ON r.company_id = c.company_id JOIN device as d ON d.device_id = r.device_id $sn_active LIMIT ? OFFSET ?)
+			UNION ALL (Select r.r_id, sn.sn_id, sn.sn, c.company_id, d.device_id from relation as r JOIN sn ON r.sn_id = sn.sn_id JOIN company as c ON r.company_id = c.company_id JOIN device as d ON d.device_id = r.device_id $d_active LIMIT ? OFFSET ?) 
+			UNION ALL (Select r.r_id, sn.sn_id, sn.sn, c.company_id, d.device_id from relation as r JOIN sn ON r.sn_id = sn.sn_id JOIN company as c ON r.company_id = c.company_id JOIN device as d ON d.device_id = r.device_id $c_active LIMIT ? OFFSET ?)";
 
 			//TODO: query without filters
 		}
 		else{
-			$sql = buildString($params);
-			array_push($params['values'], $limit);
-			array_push($params['values'], 0);
+			$sql = '(Select r.r_id, sn.sn_id, sn.sn, c.company_id, d.device_id from relation as r JOIN sn ON r.sn_id = sn.sn_id JOIN company as c ON r.company_id = c.company_id JOIN device as d ON d.device_id = r.device_id '.$sn_active.$params['p'][0].' LIMIT ? OFFSET ?)
+			UNION ALL (Select r.r_id, sn.sn_id, sn.sn, c.company_id, d.device_id from relation as r JOIN sn ON r.sn_id = sn.sn_id JOIN company as c ON r.company_id = c.company_id JOIN device as d ON d.device_id = r.device_id '.$d_active.$params['p'][1].' LIMIT ? OFFSET ?) 
+			UNION ALL (Select r.r_id, sn.sn_id, sn.sn, c.company_id, d.device_id from relation as r JOIN sn ON r.sn_id = sn.sn_id JOIN company as c ON r.company_id = c.company_id JOIN device as d ON d.device_id = r.device_id '.$c_active.$params['p'][2].' LIMIT ? OFFSET ?)';
+			
 		}
 		return $sql;
 	}
-	
+
+	function searchPrepare(&$params, $bind, $cond, $value, $limit, $offset){
+		$params['bind'] .= $bind;
+		array_push($params['p'], " $cond");
+		array_push($params['values'], $value);
+		array_push($params['values'], $limit);
+		array_push($params['values'], $offset);
+	}
+
+	function applyNoFilter(&$extra, &$params, $limit, $offset){
+		$extra += 1;
+		array_push($params['p'], "");
+		array_push($params['values'], $limit);
+		array_push($params['values'], $offset);
+		$params['bind'] .= 'ii';
+	}
 ?>
