907b63272e6aa9acc7e2b5c711c753047fb562e3
Peter Banh <peterbanhe.e@gmail.com>
2024-05-01 15:19:25 -0500

    web branch init

--- assets/php/loader/loadDevices.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$result = $db->query("Select * from `device`") or 
-		die("\r\n$data->errno : $data->error\r\n");
-	while ($data = $result->fetch_array(MYSQLI_ASSOC)){
-		$device[$data['device_id']] = $data['device'];
-	}
-?>
